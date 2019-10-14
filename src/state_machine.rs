2d3014513e85835e255fd6573d857e4f3207efb6
Matt Bell <mappum@gmail.com>
2019-10-14 13:40:25 -0700

    Change SM trait to take in &mut Store

--- src/state_machine.rs
+++ src/state_machine.rs
@@ -2,7 +2,7 @@ use crate::error::Result;
 use crate::store::Store;
 
 pub trait StateMachine<A> {
-    fn step<S: Store>(&mut self, action: A, store: S) -> Result<()>;
+    fn step<S: Store>(&mut self, action: A, store: &mut S) -> Result<()>;
 }
 
 #[cfg(test)]
@@ -14,7 +14,7 @@ mod tests {
     struct CounterSM ();
 
     impl StateMachine<u8> for CounterSM {
-        fn step<S: Store>(&mut self, n: u8, mut store: S) -> Result<()> {
+        fn step<S: Store>(&mut self, n: u8, store: &mut S) -> Result<()> {
             let count = match store.get(b"count") {
                 Err(Error(ErrorKind::Store(crate::store::Error(NotFound, _)), _)) => 0,
                 Ok(vec) => vec[0],
@@ -34,14 +34,17 @@ mod tests {
     #[test]
     fn step_counter_error() {
         let mut sm = CounterSM();
-        let store = MapStore::new();
-        assert!(sm.step(100, store).is_err());
+        let mut store = MapStore::new();
+        assert!(sm.step(100, &mut store).is_err());
     }
 
     #[test]
     fn step_counter() {
         let mut sm = CounterSM();
-        let store = MapStore::new();
-        assert!(sm.step(0, store).is_ok());
+        let mut store = MapStore::new();
+        assert!(sm.step(0, &mut store).is_ok());
+        assert!(sm.step(0, &mut store).is_err());
+        assert!(sm.step(1, &mut store).is_ok());
+        assert!(sm.step(1, &mut store).is_err());
     }
 }
