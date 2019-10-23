63c7e931b41a84e9c32c5b7c91f90c4e9bb8482e
Matt Bell <mappum@gmail.com>
2019-10-22 18:46:01 -0700

    Added compile-time tests for different store key types

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -4,10 +4,12 @@ mod error;
 mod write_cache;
 mod nullstore;
 mod key_log;
+mod splitter;
 
 pub use write_cache::WriteCache;
 pub use nullstore::NullStore;
 pub use error::{Error, ErrorKind};
+pub use splitter::Splitter;
 
 // TODO: iter method?
 
@@ -27,4 +29,27 @@ impl<S: Read + Write> Store for S {}
 
 pub trait Flush {
     fn flush<S: Store>(self, dest: &mut S) -> Result<()>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Read, NullStore};
+        
+    #[test]
+    fn fixed_length_slice_key() {
+        let key = b"0123";
+        NullStore.get(key).unwrap();
+    }
+
+    #[test]
+    fn slice_key() {
+        let key = vec![1, 2, 3, 4];
+        NullStore.get(key.as_slice()).unwrap();
+    }
+
+    #[test]
+    fn vec_key() {
+        let key = vec![1, 2, 3, 4];
+        NullStore.get(key).unwrap();
+    }
 }
