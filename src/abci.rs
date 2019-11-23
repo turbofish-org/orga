e5d1f5c3be2fa4df9b078e1c94e3bd3555c71848
cyntran <cyntran@uw.edu>
2019-11-22 17:34:50 -0800

    added RootHash as a required trait inside ABCI

--- src/abci.rs
+++ src/abci.rs
@@ -3,13 +3,13 @@ use std::net::ToSocketAddrs;
 use std::sync::mpsc::{Receiver, SyncSender, sync_channel};
 use std::sync::{Arc, Mutex, MutexGuard};
 use error_chain::bail;
-use crate::{StateMachine, Store, Flush, WriteCache, MapStore, Result, step_atomic, WriteCacheMap};
+use crate::{StateMachine, Store, RootHash, Flush, WriteCache, MapStore, Result, step_atomic, WriteCacheMap};
 
 pub use abci2::messages::abci::{Request, Response};
 use abci2::messages::abci::*;
 use abci2::messages::abci::Request_oneof_value::*;
 
-pub struct ABCIStateMachine<A: Application, S: Store> {
+pub struct ABCIStateMachine<A: Application, S: Store + RootHash> {
     app: Option<A>,
     store: S,
     receiver: Receiver<(Request, SyncSender<Response>)>,
@@ -18,7 +18,7 @@ pub struct ABCIStateMachine<A: Application, S: Store> {
     consensus_state: Option<WriteCacheMap>
 }
 
-impl<A: Application, S: Store> ABCIStateMachine<A, S> {
+impl<A: Application, S: Store + RootHash> ABCIStateMachine<A, S> {
     pub fn new(app: A, store: S) -> Self {
         let (sender, receiver) = sync_channel(0);
         ABCIStateMachine {
@@ -165,7 +165,7 @@ impl<A: Application, S: Store> ABCIStateMachine<A, S> {
 
                 let mut res = Response::new();
                 let mut message = ResponseCommit::new();
-                message.set_data(vec![0; 20]); // TODO: real merkle root
+                message.set_data(self.store.root_hash().to_vec());
                 res.set_commit(message);
                 Ok(res)
             },
