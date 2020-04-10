e9d329984e8f9fe50803f7010e69ccd2170b66fc
Matt Bell <mappum@gmail.com>
2020-04-10 16:02:03 -0700

    Added Encode and Decode impl for Option

--- src/encoding/mod.rs
+++ src/encoding/mod.rs
@@ -1,5 +1,5 @@
 use std::io::{Read, Write};
-use failure::bail;
+use failure::{bail, format_err};
 use crate::Result;
 
 pub trait Encode {
@@ -46,10 +46,50 @@ int_impl!(u8, 1);
 int_impl!(u16, 2);
 int_impl!(u32, 4);
 int_impl!(u64, 8);
+int_impl!(u128, 16);
 int_impl!(i8, 1);
 int_impl!(i16, 2);
 int_impl!(i32, 4);
 int_impl!(i64, 8);
+int_impl!(i128, 16);
+
+impl<T: Encode> Encode for Option<T> {
+    fn encode_into<W: Write>(&self, dest: &mut W) -> Result<()> {
+        match self {
+            None => {
+                dest.write_all(&[0])
+                    .map_err(|err| format_err!("{}", err))
+            },
+            Some(value) => {
+                dest.write_all(&[1])
+                    .map_err(|err| format_err!("{}", err))?;
+                value.encode_into(dest)
+            }
+        }
+    }
+
+    fn encoding_length(&self) -> Result<usize> {
+        match self {
+            None => Ok(1),
+            Some(value) => Ok(1 + value.encoding_length()?)
+        }
+    }
+}
+
+impl<T: Decode> Decode for Option<T> {
+    fn decode<R: Read>(mut input: R) -> Result<Self> {
+        let mut byte = [0; 1];
+        input.read_exact(&mut byte[..])?;
+
+        let value = match byte[0] {
+            0 => None,
+            1 => Some(T::decode(input)?),
+            byte => bail!("Unexpected byte {}", byte)
+        };
+
+        Ok(value)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -71,5 +111,20 @@ mod tests {
         assert_eq!(bytes.as_slice(), &[0, 0, 0, 0x12, 0x34, 0x56, 0x78, 0x90]);
         let decoded_value = u64::decode(bytes.as_slice()).unwrap();
         assert_eq!(decoded_value, value);
+    }
+
+    #[test]
+    fn encode_decode_option() {
+        let value = Some(0x1234567890u64);
+        let bytes = value.encode().unwrap();
+        assert_eq!(bytes.as_slice(), &[1, 0, 0, 0, 0x12, 0x34, 0x56, 0x78, 0x90]);
+        let decoded_value: Option<u64> = Decode::decode(bytes.as_slice()).unwrap();
+        assert_eq!(decoded_value, value);
+
+        let value: Option<u64> = None;
+        let bytes = value.encode().unwrap();
+        assert_eq!(bytes.as_slice(), &[0]);
+        let decoded_value: Option<u64> = Decode::decode(bytes.as_slice()).unwrap();
+        assert_eq!(decoded_value, None);
     }
 }
