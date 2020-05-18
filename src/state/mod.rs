bded3377279f519715a88368f129b18d3f0fedf5
Matt Bell <mappum@gmail.com>
2020-05-18 16:29:24 -0700

    Wrapper -> WrapperStore

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -4,7 +4,7 @@ mod value;
 mod wrapper;
 
 pub use value::Value;
-pub use wrapper::Wrapper;
+pub use wrapper::WrapperStore;
 
 pub trait State<S: Store>: Sized {
     fn wrap_store(store: S) -> Result<Self>;
--- src/state/wrapper.rs
+++ src/state/wrapper.rs
@@ -3,22 +3,22 @@ use crate::error::Result;
 use crate::store::Store;
 use std::ops::{Deref, DerefMut};
 
-pub struct Wrapper<S: Store>(S);
+pub struct WrapperStore<S: Store>(S);
 
-impl<S: Store> State<S> for Wrapper<S> {
-    fn wrap_store(store: S) -> Result<Wrapper<S>> {
-        Ok(Wrapper(store))
+impl<S: Store> State<S> for WrapperStore<S> {
+    fn wrap_store(store: S) -> Result<WrapperStore<S>> {
+        Ok(WrapperStore(store))
     }
 }
 
-impl<S: Store> Deref for Wrapper<S> {
+impl<S: Store> Deref for WrapperStore<S> {
     type Target = S;
     fn deref(&self) -> &S {
         &self.0
     }
 }
 
-impl<S: Store> DerefMut for Wrapper<S> {
+impl<S: Store> DerefMut for WrapperStore<S> {
     fn deref_mut(&mut self) -> &mut S {
         &mut self.0
     }
