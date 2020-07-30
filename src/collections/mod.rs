2f05299d3b7eed19ebc522f8047ed1310ba2de16
Judd Keppel <kepzorz@gmail.com>
2020-07-29 18:58:15 -0700

    add entry trait
    Co-authored-by: Matt Bell <mappum@gmail.com>

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -5,3 +5,12 @@ mod set;
 pub use deque::Deque;
 pub use map::Map;
 pub use set::Set;
+
+use crate::{Decode, Encode};
+pub trait Entry {
+    type Key: Encode + Decode;
+    type Value: Encode + Decode;
+
+    fn into_entry(self) -> (Self::Key, Self::Value);
+    fn from_entry(entry: (Self::Key, Self::Value)) -> Self;
+}
