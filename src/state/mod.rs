c14dd70e4b39e0701deec0b92824261f93a9493b
Matt Bell <mappum@gmail.com>
2020-08-16 17:37:19 -0700

    Only require Read for wrapped store in State

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,4 +1,5 @@
-use crate::{Result, Store};
+use crate::Result;
+use crate::store::Read;
 
 mod value;
 mod wrapper;
@@ -8,6 +9,6 @@ pub use wrapper::WrapperStore;
 
 /// A trait for types which provide a higher-level API for data stored within a
 /// [`store::Store`](../store/trait.Store.html).
-pub trait State<S: Store>: Sized {
+pub trait State<S: Read>: Sized {
     fn wrap_store(store: S) -> Result<Self>;
 }
