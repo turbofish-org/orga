14abbef86c6e19779890f9ee16bc5ea3f2958f1e
Matt Bell <mappum@gmail.com>
2021-06-04 19:18:01 -0500

    Added store::Iter test coverage

--- src/store/iter.rs
+++ src/store/iter.rs
@@ -2,7 +2,14 @@ use crate::store::{Read, KV};
 use crate::Result;
 use std::ops::{Bound, RangeBounds};
 
-// TODO: make reversible
+// TODO: make reversible (requires `get_prev` method on Read)
+
+// TODO: should we continue attempting to read for iterations after reaching the
+// end of store data if the end has not been reached? (e.g. kill `done`
+// property). this will not usually happen since the data won't be mutated while
+// the shared reference to parent exists, unless a store uses interior
+// mutability
+
 pub struct Iter<'a, S: ?Sized> {
     parent: &'a S,
     bounds: (Bound<Vec<u8>>, Bound<Vec<u8>>),
@@ -69,5 +76,144 @@ impl<'a, S: Read> Iterator for Iter<'a, S> {
                 None
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::store::{Write, MapStore};
+
+    fn test_store() -> MapStore {
+        let mut store = MapStore::new();
+        store.put(vec![0], vec![0]).unwrap();
+        store.put(vec![1], vec![1]).unwrap();
+        store.put(vec![2], vec![2]).unwrap();
+        store
+    }
+
+    #[test]
+    fn iter_unbounded_unbounded() {
+        let store = test_store();
+        let mut iter = Iter {
+            parent: &store,
+            bounds: (Bound::Unbounded, Bound::Unbounded),
+            done: false,
+        };
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![0], vec![0]));
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![1], vec![1]));
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![2], vec![2]));
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn iter_included_existing() {
+        let store = test_store();
+        let mut iter = Iter {
+            parent: &store,
+            bounds: (Bound::Included(vec![0]), Bound::Unbounded),
+            done: false,
+        };
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![0], vec![0]));
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![1], vec![1]));
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![2], vec![2]));
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn iter_included_nonexistent() {
+        let store = test_store();
+        let mut iter = Iter {
+            parent: &store,
+            bounds: (Bound::Included(vec![0, 1]), Bound::Unbounded),
+            done: false,
+        };
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![1], vec![1]));
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![2], vec![2]));
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn iter_excluded_existing() {
+        let store = test_store();
+        let mut iter = Iter {
+            parent: &store,
+            bounds: (Bound::Excluded(vec![0]), Bound::Unbounded),
+            done: false,
+        };
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![1], vec![1]));
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![2], vec![2]));
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn iter_excluded_nonexistent() {
+        let store = test_store();
+        let mut iter = Iter {
+            parent: &store,
+            bounds: (Bound::Excluded(vec![0, 1]), Bound::Unbounded),
+            done: false,
+        };
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![1], vec![1]));
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![2], vec![2]));
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn iter_error() {
+        struct ErrorStore;
+        impl Read for ErrorStore {
+            fn get(&self, _key: &[u8]) -> Result<Option<Vec<u8>>> {
+                failure::bail!("get")
+            }
+
+            fn get_next(&self, _key: &[u8]) -> Result<Option<KV>> {
+                failure::bail!("get_next")
+            }
+        }
+
+        let mut iter = Iter {
+            parent: &ErrorStore,
+            bounds: (Bound::Unbounded, Bound::Unbounded),
+            done: false,
+        }; 
+        assert_eq!(
+            iter.next().unwrap().unwrap_err().to_string(),
+            "get",
+        );
+
+        let mut iter = Iter {
+            parent: &ErrorStore,
+            bounds: (Bound::Excluded(vec![]), Bound::Unbounded),
+            done: false,
+        };
+        assert_eq!(
+            iter.next().unwrap().unwrap_err().to_string(),
+            "get_next",
+        );
+    }
+
+    #[test]
+    fn iter_end_past_range() {
+        let store = test_store();
+        let mut iter = Iter {
+            parent: &store,
+            bounds: (Bound::Unbounded, Bound::Included(vec![1])),
+            done: false,
+        };
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![0], vec![0]));
+        assert_eq!(iter.next().unwrap().unwrap(), (vec![1], vec![1]));
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn iter_done() {
+        let store = test_store();
+        let mut iter = Iter {
+            parent: &store,
+            bounds: (Bound::Unbounded, Bound::Unbounded),
+            done: true,
+        };
+        assert!(iter.next().is_none());
     }
 }
