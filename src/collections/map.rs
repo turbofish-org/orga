20079b3d35cb209711b161962380aa70e5319248
Matt Bell <mappum@gmail.com>
2021-06-06 15:10:23 -0500

    Added doc comments for collections::Map

--- src/collections/map.rs
+++ src/collections/map.rs
@@ -8,6 +8,15 @@ use crate::store::*;
 use crate::Result;
 use ed::*;
 
+/// A map collection which stores data in a backing key/value store.
+///
+/// Keys are encoded into bytes and values are stored at the resulting key, with
+/// child key/value entries (if any) stored with the encoded key as their
+/// prefix.
+///
+/// When values in the map are mutated, inserted, or deleted, they are retained
+/// in an in-memory map until the call to `State::flush` which writes the
+/// changes to the backing store.
 pub struct Map<K, V, S = DefaultBackingStore> {
     store: Store<S>,
     children: HashMap<K, Option<V>>,
@@ -43,6 +52,12 @@ where
     V: State<S>,
     S: Read,
 {
+    /// Gets a reference to the value in the map for the given key, or `None` if
+    /// the key has no value.
+    ///
+    /// The returned value will reference the latest changes to the data even if
+    /// the value was inserted, modified, or deleted since the last time the map
+    /// was flushed.
     pub fn get<B: Borrow<K>>(&self, key: B) -> Result<Option<Child<V>>> {
         let key = key.borrow();
         Ok(if self.children.contains_key(key) {
@@ -59,10 +74,20 @@ where
         }) 
     }
 
+    /// Gets a mutable reference to the value in the map for the given key, or
+    /// `None` if the key has no value.
+    ///
+    /// If the value is mutated, it will be retained in memory until the map is
+    /// flushed.
+    ///
+    /// The returned value will reference the latest changes to the data even if
+    /// the value was inserted, modified, or deleted since the last time the map
+    /// was flushed.
     pub fn get_mut(&mut self, key: K) -> Result<Option<ChildMut<K, V, S>>> {
         Ok(self.entry(key)?.into())
     }
 
+    /// Returns a mutable reference to the key/value entry for the given key.
     pub fn entry(&mut self, key: K) -> Result<Entry<K, V, S>> {
         Ok(if self.children.contains_key(&key) {
             // value is already retained in memory (was modified)
@@ -85,6 +110,9 @@ where
         })
     }
 
+    /// Gets the value from the key/value store by reading and decoding from raw
+    /// bytes, then constructing a `State` instance for the value by creating a
+    /// substore which uses the key as a prefix.
     fn get_from_store(&self, key: &K) -> Result<Option<V>> {
         let key_bytes = key.encode()?;
         self.store
@@ -99,6 +127,7 @@ where
 }
 
 impl<K: Hash + Eq, V, S> Map<K, V, S> {
+    /// Removes the value at the given key, if any.
     pub fn remove(&mut self, key: K) {
         self.children.insert(key, None);
     }
@@ -110,6 +139,12 @@ where
     V: State<S>,
     S: Write,
 {
+    /// Removes all values with the given prefix from the key/value store.
+    /// Iterates until reaching the first key that does not have the given
+    /// prefix, or the end of the store.
+    ///
+    /// This method is used to delete a child value and all of its child entries
+    /// (if any).
     fn remove_from_store(store: &mut Store<S>, prefix: &[u8]) -> Result<bool> {
         let entries = store.range(prefix.to_vec()..);
         // TODO: make so we don't have to collect (should be able to delete
@@ -130,6 +165,11 @@ where
         Ok(exists)
     }
 
+    /// Writes a change to the key/value store for the given key. If
+    /// `maybe_value` is `Some`, the value's `State::flush` implementation is
+    /// called then its binary encoding is written to `key`. If `maybe_value` is
+    /// `None`, the value is removed by deleting all entries which start with
+    /// `key`.
     fn apply_change(store: &mut Store<S>, key: &K, maybe_value: Option<V>) -> Result<()> {
         let key_bytes = key.encode()?;
         
@@ -149,8 +189,13 @@ where
     }
 }
 
+/// An immutable reference to an existing value in a collection.
 pub enum Child<'a, V> {
+    /// An existing value which was loaded from the store.
     Unmodified(V),
+
+    /// A reference to an existing value which is being retained in memory
+    /// because it was modified.
     Modified(&'a V),
 }
 
@@ -171,88 +216,22 @@ impl<'a, V: Default> Default for Child<'a, V> {
     }
 }
 
-pub enum Entry<'a, K, V, S> {
-    Vacant {
-        key: K,
-        parent: &'a mut Map<K, V, S>,
-    },
-    Occupied {
-        child: ChildMut<'a, K, V, S>,
-    },
-}
-
-impl<'a, K, V, S> Entry<'a, K, V, S>
-where
-    K: Encode + Terminated + Eq + Hash,
-    V: State<S>,
-    S: Read,
-{
-    pub fn or_create(self, value: V::Encoding) -> Result<ChildMut<'a, K, V, S>> {
-        Ok(match self {
-            Entry::Vacant { key, parent } => {
-                let key_bytes = key.encode()?;
-                let substore = parent.store.sub(key_bytes.as_slice());
-                let value = V::create(substore, value)?;
-                ChildMut::Unmodified(Some((key, value, parent)))
-            }
-            Entry::Occupied { child } => child,
-        })
-    }
-
-    pub fn or_insert(self, value: V::Encoding) -> Result<ChildMut<'a, K, V, S>> {
-        let mut child = self.or_create(value)?;
-        child.deref_mut();
-        Ok(child)
-    }
-}
-
-impl<'a, K, V, S> Entry<'a, K, V, S>
-where
-    K: Encode + Terminated + Eq + Hash,
-    S: Write,
-{
-    pub fn remove(self) -> Result<bool> {
-        Ok(match self {
-            Entry::Occupied { child } => {
-                child.remove();
-                true
-            },
-            Entry::Vacant { .. } => false,
-        })
-    }
-}
-
-impl<'a, K, V, S, D> Entry<'a, K, V, S>
-where
-    K: Encode + Terminated + Eq + Hash,
-    V: State<S, Encoding = D>,
-    S: Read,
-    D: Default,
-{
-    pub fn or_default(self) -> Result<ChildMut<'a, K, V, S>> {
-        self.or_create(D::default())
-    }
-
-    pub fn or_insert_default(self) -> Result<ChildMut<'a, K, V, S>> {
-        self.or_insert(D::default())
-    }
-}
-
-impl<'a, K, V, S> From<Entry<'a, K, V, S>> for Option<ChildMut<'a, K, V, S>> {
-    fn from(entry: Entry<'a, K, V, S>) -> Self {
-        match entry {
-            Entry::Vacant { .. } => None,
-            Entry::Occupied { child } => Some(child),
-        }
-    }
-}
-
+/// A mutable reference to an existing value in a collection.
+///
+/// If the value is mutated, it will be retained in memory until the parent
+/// collection is flushed.
 pub enum ChildMut<'a, K, V, S> {
+    /// An existing value which was loaded from the store.
     Unmodified(Option<(K, V, &'a mut Map<K, V, S>)>),
+
+    /// A mutable reference to an existing value which is being retained in
+    /// memory because it was modified.
     Modified(hash_map::OccupiedEntry<'a, K, Option<V>>),
 }
 
 impl<'a, K: Hash + Eq, V, S> ChildMut<'a, K, V, S> {
+    /// Removes the value and all of its child key/value entries (if any) from
+    /// the parent collection.
     pub fn remove(self) {
         match self {
             ChildMut::Unmodified(mut inner) => {
@@ -297,6 +276,123 @@ where
         }
     }
 }
+
+/// A mutable reference to a key/value entry in a collection, which may be
+/// empty.
+pub enum Entry<'a, K, V, S> {
+    /// References an entry in the collection which does not have a value.
+    Vacant {
+        key: K,
+        parent: &'a mut Map<K, V, S>,
+    },
+
+    /// Referendes an entry in the collection which has a value.
+    Occupied {
+        child: ChildMut<'a, K, V, S>,
+    },
+}
+
+impl<'a, K, V, S> Entry<'a, K, V, S>
+where
+    K: Encode + Terminated + Eq + Hash,
+    V: State<S>,
+    S: Read,
+{
+    /// If the `Entry` is empty, this method creates a new instance based on the
+    /// given data. If not empty, this method returns a mutable reference to the
+    /// existing data.
+    ///
+    /// Note that if a new instance is created, it will not be written to the
+    /// store during the flush step unless the value gets modified. See
+    /// `or_insert` for a variation which will always write the newly created
+    /// value.
+    pub fn or_create(self, data: V::Encoding) -> Result<ChildMut<'a, K, V, S>> {
+        Ok(match self {
+            Entry::Vacant { key, parent } => {
+                let key_bytes = key.encode()?;
+                let substore = parent.store.sub(key_bytes.as_slice());
+                let value = V::create(substore, data)?;
+                ChildMut::Unmodified(Some((key, value, parent)))
+            }
+            Entry::Occupied { child } => child,
+        })
+    }
+
+    /// If the `Entry` is empty, this method creates a new instance based on the
+    /// given data. If not empty, this method returns a mutable reference to the
+    /// existing data.
+    ///
+    /// Note that if a new instance is created, it will always be written to the
+    /// store during the flush step even if the value never gets modified. See
+    /// `or_create` for a variation which will only write the newly created
+    /// value if it gets modified.
+    pub fn or_insert(self, data: V::Encoding) -> Result<ChildMut<'a, K, V, S>> {
+        let mut child = self.or_create(data)?;
+        child.deref_mut();
+        Ok(child)
+    }
+}
+
+impl<'a, K, V, S> Entry<'a, K, V, S>
+where
+    K: Encode + Terminated + Eq + Hash,
+    S: Write,
+{
+    /// Removes the value for the `Entry` if it exists. Returns a boolean which
+    /// is `true` if a value previously existed for the entry, `false`
+    /// otherwise.
+    pub fn remove(self) -> Result<bool> {
+        Ok(match self {
+            Entry::Occupied { child } => {
+                child.remove();
+                true
+            },
+            Entry::Vacant { .. } => false,
+        })
+    }
+}
+
+impl<'a, K, V, S, D> Entry<'a, K, V, S>
+where
+    K: Encode + Terminated + Eq + Hash,
+    V: State<S, Encoding = D>,
+    S: Read,
+    D: Default,
+{
+    /// If the `Entry` is empty, this method creates a new instance based on the
+    /// default for the value's data encoding. If not empty, this method returns
+    /// a mutable reference to the existing data.
+    ///
+    /// Note that if a new instance is created, it will not be written to the
+    /// store during the flush step unless the value gets modified. See
+    /// `or_insert_default` for a variation which will always write the newly
+    /// created value. 
+    pub fn or_default(self) -> Result<ChildMut<'a, K, V, S>> {
+        self.or_create(D::default())
+    }
+
+    /// If the `Entry` is empty, this method creates a new instance based on the
+    /// default for the value's data encoding. If not empty, this method returns
+    /// a mutable reference to the existing data.
+    ///
+    /// Note that if a new instance is created, it will always be written to the
+    /// store during the flush step even if the value never gets modified. See
+    /// `or_default` for a variation which will only write the newly created
+    /// value if it gets modified.
+    pub fn or_insert_default(self) -> Result<ChildMut<'a, K, V, S>> {
+        self.or_insert(D::default())
+    }
+}
+
+impl<'a, K, V, S> From<Entry<'a, K, V, S>> for Option<ChildMut<'a, K, V, S>> {
+    fn from(entry: Entry<'a, K, V, S>) -> Self {
+        match entry {
+            Entry::Vacant { .. } => None,
+            Entry::Occupied { child } => Some(child),
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
