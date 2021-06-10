f27a9736e24b12d1ee63d1b661a09de4c060630e
Charlie Little <cwlittle@utexas.edu>
2021-06-09 19:16:18 -0500

    Uncomment previous ProofBuilder logic

--- src/merk/proofbuilder.rs
+++ src/merk/proofbuilder.rs
@@ -1,63 +1,58 @@
-// use std::cell::RefCell;
-// use std::collections::BTreeSet;
-// use std::ops::{RangeInclusive, Bound};
-// use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::BTreeSet;
+use std::ops::{RangeInclusive, Bound};
+use std::rc::Rc;
 
-// use crate::Result;
-// use crate::store;
-// use super::MerkStore;
+use crate::Result;
+use crate::store;
+use super::MerkStore;
+use merk::proofs::query::Query;
 
-// /// Records reads to a `MerkStore` and uses them to build a proof including all
-// /// accessed keys.
-pub struct ProofBuilder {
-    // store: &'a MerkStore<'a>,
-    // query: Rc<RefCell<Query>>,
+/// Records reads to a `MerkStore` and uses them to build a proof including all
+/// accessed keys.
+pub struct ProofBuilder<'a> {
+    store: &'a MerkStore<'a>,
+    query: Rc<RefCell<Query>>,
 }
 
-// impl<'a> ProofBuilder<'a> {
-//     /// Constructs a `ProofBuilder` which provides read access to data in the
-//     /// given `MerkStore`.
-//     pub fn new(store: &'a MerkStore<'a>) -> Self {
-//         ProofBuilder {
-//             store,
-//             query: Rc::new(RefCell::new(Query::new()))
-//         }
-//     }
- 
-//     /// Builds a Merk proof including all the data accessed during the life of
-//     /// the `ProofBuilder`.
-//     pub fn build(self) -> Result<Vec<u8>> {
-//         let query = self.query.take();
-//         self.store.merk().prove(query)
-//     }
-// }
+impl<'a> ProofBuilder<'a> {
+    /// Constructs a `ProofBuilder` which provides read access to data in the
+    /// given `MerkStore`.
+    pub fn new(store: &'a MerkStore<'a>) -> Self {
+        ProofBuilder {
+            store,
+            query: Rc::new(RefCell::new(Query::new()))
+        }
+    }
 
-// impl<'a> store::Read for ProofBuilder<'a> {
-//     /// Gets the value from the underlying store, recording the key to be
-//     /// included in the proof when `build` is called.
-//     fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>> {
-//         self.query
-//             .borrow_mut()
-//             .insert_key(key.to_vec());
+    /// Builds a Merk proof including all the data accessed during the life of
+    /// the `ProofBuilder`.
+    pub fn build(self) -> Result<Vec<u8>> {
+        let query = self.query.take();
+        self.store.merk().prove(query)
+    }
+}
 
-//         self.store.get(key)
-//     }
+impl<'a> store::Read for ProofBuilder<'a> {
+    /// Gets the value from the underlying store, recording the key to be
+    /// included in the proof when `build` is called.
+    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>> {
+        self.query
+            .borrow_mut()
+            .insert_key(key.to_vec());
 
-//     fn get_next(&self, key: &[u8]) -> Result<Option<store::KV>> {
-//         let maybe_entry = self.store
-//             .get_next(key)?
-//             .map(|(next_key, value)| {
-//                 // TODO: support inserting `(Bound, Bound)` into query
-//                 let range = key.to_vec()..=next_key.to_vec();
-//                 self.query
-//                     .borrow_mut()
-//                     .insert_range_inclusive(range);
+        self.store.get(key)
+    }
 
-//                 (next_key.to_vec(), value.to_vec())
-//             });
-//         Ok(maybe_entry)
-//     }
-// }
+    fn get_next(&self, key: &[u8]) -> Result<Option<store::KV>> {
+        let maybe_entry = self.store
+            .get_next(key)?
+            .map(|(next_key, value)| {
+                // TODO: support inserting `(Bound, Bound)` into query
+                let range = key.to_vec()..=next_key.to_vec();
+                self.query
+                    .borrow_mut()
+                    .insert_range_inclusive(range);
 
 // #[cfg(test)]
 // mod tests {
