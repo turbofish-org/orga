30edbeff332d4354fde4d5031068adf64c7d50d1
Charlie Little <cwlittle@utexas.edu>
2021-07-20 15:59:42 -0500

    Add test for map range bounded non-inclusive

--- src/collections/map.rs
+++ src/collections/map.rs
@@ -1201,4 +1201,22 @@ mod tests {
 
         assert_eq!(actual, expected);
     }
+
+    #[test]
+    fn map_range_map_only_bounded_non_inclusive() {
+        let store = Store::new(MapStore::new());
+        let mut map: Map<u32, u32> = Map::create(store.clone(), ()).unwrap();
+
+        map.entry(12).unwrap().or_insert(24).unwrap();
+        map.entry(13).unwrap().or_insert(26).unwrap();
+        map.entry(14).unwrap().or_insert(28).unwrap();
+
+        let mut actual: Vec<(u32, u32)> = Vec::with_capacity(3);
+
+        map.range(12..14).for_each(|(k, v)| actual.push((k, *v)));
+
+        let expected: Vec<(u32, u32)> = vec![(12, 24), (13, 26)];
+
+        assert_eq!(actual, expected);
+    }
 }
