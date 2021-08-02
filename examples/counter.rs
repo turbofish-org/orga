ec256155b1aa09d669a50be5ec3e64f57ac7fc83
Charlie Little <cwlittle@utexas.edu>
2021-08-02 15:27:24 -0500

    Experimental changes with manual State derivation

--- examples/counter.rs
+++ examples/counter.rs
@@ -7,18 +7,53 @@ use orga::store::{BufStore, DefaultBackingStore, NullStore, Read, Store, Write};
 
 struct App;
 
-#[derive(State)]
+//#[derive(State)]
 struct CounterState {
     count: u32,
+    count_map: orga::collections::Map<u32, u32>,
 }
 
-fn foo<S: Read + Write>(store: S) {
-    let mut store = Store::new(store);
-    let mut store = Store::new(BufStore::wrap(store));
-    let state: CounterState = CounterState::create(store, Default::default()).unwrap();
+impl<S> State<S> for CounterState {
+    type Encoding = (
+        <u32 as ::orga::state::State<S>>::Encoding,
+        <orga::collections::Map<u32, u32> as ::orga::state::State<S>>::Encoding,
+    );
+
+    fn create(store: Store<S>, data: Self::Encoding) -> orga::Result<Self>
+    where
+        S: Read,
+    {
+        Ok(CounterState {
+            count: ::orga::state::State::create(store.sub(&[0]), data.0)?,
+            count_map: orga::collections::Map::create(store.sub(&[0]), ())?,
+        })
+    }
+
+    fn flush(self) -> ::orga::Result<Self::Encoding>
+    where
+        S: Write,
+    {
+        let flushed_value: ::orga::Result<u32> = State::<S>::flush(self.count);
+        Ok((flushed_value?,))
+    }
 }
 
-fn main() {}
+impl From<CounterState> for <CounterState as ::orga::state::State>::Encoding {
+    fn from(state: CounterState) -> Self {
+        (state.count.into(), state.count_map.into())
+    }
+}
+
+fn foo<S: Read>(store: Store<&mut BufStore<&mut BufStore<&mut S>>>) {
+    let _state: CounterState = CounterState::create(store, Default::default()).unwrap();
+}
+
+fn main() {
+    let store: Store<DefaultBackingStore> = Store::new(MerkStore::new(std::path::PathBuf::new()));
+    // let mut_store: &mut BufStore<&mut BufStore<&mut S>> = BufStore::new(BufStore::new());
+
+    // foo(store);
+}
 
 // impl Application<DefaultBackingStore> for App {
 //     fn init_chain(
@@ -29,17 +64,16 @@ fn main() {}
 //         Ok(Default::default())
 //     }
 
-//     fn deliver_tx(
-//         &self,
-//         store: DefaultBackingStore,
-//         req: tendermint_proto::abci::RequestDeliverTx,
-//     ) -> orga::Result<tendermint_proto::abci::ResponseDeliverTx> {
-//         let state: CounterState = CounterState::create(store, Default::default()).unwrap();
-//         state.count += 1;
-//         state.flush();
-//         Ok(Default::default())
-//     }
-//     /*
+// fn deliver_tx(
+//     &self,
+//     store: DefaultBackingStore,
+//     req: tendermint_proto::abci::RequestDeliverTx,
+// ) -> orga::Result<tendermint_proto::abci::ResponseDeliverTx> {
+//     let state: CounterState = CounterState::create(store, Default::default()).unwrap();
+//     state.count += 1;
+//     state.flush();
+//     Ok(Default::default())
+// }
 //     fn begin_block(
 //         &self,
 //         store: MerkStore,
