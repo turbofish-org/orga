13e971973074007cb1841caf3363a97503bb9f69
Charlie Little <cwlittle@utexas.edu>
2021-08-11 16:42:00 -0500

    Clippy: fix useless conversion

--- examples/counter.rs
+++ examples/counter.rs
@@ -72,7 +72,7 @@ impl Application for App {
 fn main() {
     let app = App {};
     let sm_dir = "./counter.sm";
-    if Path::new(sm_dir.into()).is_dir() {
+    if Path::new(sm_dir).is_dir() {
         fs::remove_dir_all(Path::new(sm_dir)).unwrap();
     }
     let store = orga::merk::MerkStore::new("./counter.sm".into());
