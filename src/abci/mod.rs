99e35c7614618fd53ce6976d40719e7b5a449d67
Charlie Little <cwlittle@utexas.edu>
2021-08-18 14:17:19 -0500

    Clippy fix: field reassign with default

--- src/abci/mod.rs
+++ src/abci/mod.rs
@@ -279,8 +279,8 @@ impl<A: Application> ABCIStateMachine<A> {
             Req::ListSnapshots(_req) => {
                 let self_store = self.store.as_mut().unwrap();
                 let snapshots = self_store.borrow_mut().list_snapshots()?;
-                let mut res = ResponseListSnapshots::default();
-                res.snapshots = snapshots;
+                let res = ResponseListSnapshots { snapshots };
+
                 Ok(Res::ListSnapshots(res))
             }
             Req::OfferSnapshot(req) => {
