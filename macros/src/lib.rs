7efedcfa90b561456bbc1309284a66fa211c6a2c
Charlie Little <cwlittle@utexas.edu>
2021-08-20 13:40:06 -0500

    Add derive next implementation

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
 
 mod entry;
+mod next;
 mod state;
 
 #[proc_macro_derive(State)]
@@ -11,4 +12,9 @@ pub fn state(item: TokenStream) -> TokenStream {
 #[proc_macro_derive(Entry, attributes(key))]
 pub fn entry(item: TokenStream) -> TokenStream {
     entry::derive(item)
+}
+
+#[proc_macro_derive(Next)]
+pub fn next(item: TokenStream) -> TokenStream {
+    state::derive(item)
 }
