6b744830ff0b55bbd40d123659bda78bfdd13e73
Matt Bell <mappum@gmail.com>
2021-09-07 13:10:50 -0500

    Add simple compile-time Query derive tests

---
+++ tests/derive_query.rs
@@ -0,0 +1,51 @@
+#![feature(specialization)]
+#![feature(trivial_bounds)]
+
+use orga::query::Query;
+use orga::collections::Deque;
+
+#[derive(Query)]
+struct Foo<T> {
+    a: u8,
+    b: Option<T>,
+    bar: Bar,
+}
+
+#[derive(Query)]
+struct Bar {
+    deque: Deque<u32>,
+}
+
+impl<T> Foo<T> {
+    #[query]
+    pub fn basic(&self) {}
+
+    #[query]
+    pub fn input_and_output(&self, n: u32) -> u32 {
+        self.a as u32 + n
+    }
+
+    #[query]
+    pub fn generic_input(&self, _t: T) -> u32 {
+        123
+    }
+
+    #[query]
+    pub fn complex_type(&self) -> orga::Result<u32> {
+        let res = self.bar.deque.get(123)?
+            .unwrap_or_default();
+        Ok(*res)
+    }
+}
+
+impl<T: Clone + Default> Foo<T> {
+    #[query]
+    pub fn generic_output(&self) -> T {
+        self.b.clone().unwrap_or_default()
+    }
+
+    #[query]
+    pub fn wrapped_generic_output(&self) -> Option<T> {
+        self.b.clone()
+    }
+}
