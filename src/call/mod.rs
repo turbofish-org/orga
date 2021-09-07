333d4a97911399be1fd95f6197048fdce6655214
Matt Bell <mappum@gmail.com>
2021-09-07 18:26:23 -0500

    Add Call implementations

--- src/call/mod.rs
+++
@@ -1,9 +0,0 @@
-use crate::Result;
-use ed::{Decode, Encode};
-
-pub trait Call {
-    type Call: Encode + Decode;
-    // TODO: type Res: Encode + Decode;
-
-    fn call(&mut self, call: Self::Call) -> Result<()>;
-}
