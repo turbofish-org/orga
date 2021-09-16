3f6cc0781ac7c64fc419ab450da545d528afeaca
Charlie Little <cwlittle@utexas.edu>
2021-09-16 11:16:10 -0500

    Add BitcoinSPV State implementation

--- src/collections/bitcoin_spv.rs
+++ src/collections/bitcoin_spv.rs
@@ -1,6 +1,100 @@
 use super::Deque;
+use crate::state::State;
+use crate::store::DefaultBackingStore;
+use crate::store::Store;
+use crate::Result;
+use bitcoin::consensus::{Decodable, Encodable};
 use bitcoin::BlockHeader;
+use ed::{Decode, Encode};
+use std::io::{Read, Write};
+
+pub struct SPVBlockHeader {
+    inner: BlockHeader,
+}
+
+// impl SPVBlockHeader {
+//     fn refresh_encoding_data(&mut self) -> Result<()> {
+//         let mut dest: Vec<u8> = Vec::new();
+//         match self.inner.consensus_encode(dest) {
+//             Ok(length) => {
+//                 self.encoding_length = length;
+//                 self.encoded_bytes = dest;
+//                 Ok(())
+//             }
+//             Err(e) => Err(e.into()),
+//         }
+//     }
+
+//     fn encoded_bytes(&self) -> Result<Vec<u8>> {
+//         self.refresh_encoding_data()?;
+//         Ok(self.encoded_bytes)
+//     }
+
+//     fn encoding_length(&self) -> Result<usize> {
+//         self.refresh_encoding_data()?;
+//         Ok(self.encoding_length)
+//     }
+// }
+
+impl Encode for SPVBlockHeader {
+    fn encode(&self) -> ed::Result<Vec<u8>> {
+        let mut dest: Vec<u8> = Vec::new();
+        self.encode_into(&mut dest)?;
+        Ok(dest)
+    }
+
+    fn encode_into<W: Write>(&self, mut dest: &mut W) -> ed::Result<()> {
+        let mut dest: Vec<u8> = Vec::new();
+        match self.inner.consensus_encode(dest) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e.into()),
+        }
+    }
+
+    fn encoding_length(&self) -> ed::Result<usize> {
+        let mut _dest: Vec<u8> = Vec::new();
+        match self.inner.consensus_encode(_dest) {
+            Ok(inner) => Ok(inner),
+            Err(e) => Err(e.into()),
+        }
+    }
+}
+
+impl Decode for SPVBlockHeader {
+    fn decode<R: Read>(input: R) -> ed::Result<Self> {
+        let decoded_bytes: std::result::Result<BlockHeader, bitcoin::consensus::encode::Error> =
+            Decodable::consensus_decode(input);
+        match decoded_bytes {
+            Ok(header) => Ok(Self { inner: header }),
+            Err(e) => {
+                let std_e =
+                    std::io::Error::new(std::io::ErrorKind::Other, "Failed to decode header");
+                Err(std_e.into())
+            }
+        }
+    }
+}
 
 pub struct BitcoinSPV {
-    deque: Deque<BlockHeader>,
+    deque: Deque<SPVBlockHeader>,
 }
+
+impl From<BitcoinSPV> for () {
+    fn from(_spv: BitcoinSPV) {}
+}
+
+impl State<DefaultBackingStore> for BitcoinSPV {
+    type Encoding = ();
+
+    fn create(store: Store<DefaultBackingStore>, data: Self::Encoding) -> Result<Self> {
+        Ok(BitcoinSPV {
+            deque: Deque::create(store, Default::default())?,
+        })
+    }
+
+    fn flush(self) -> Result<Self::Encoding> {
+        Ok(())
+    }
+}
+
+impl BitcoinSPV {}
