bb8c07f738010e806dbebd0b7cccd18164000606
Matt Bell <mappum@gmail.com>
2021-09-17 14:45:23 -0500

    Removed scratch client query code

--- src/client.rs
+++ src/client.rs
@@ -1,12 +1,26 @@
-use crate::Result;
+use std::cell::RefCell;
+use std::ops::{Deref, Not};
+use std::rc::Rc;
 
-pub trait Client<T> {
-    type Client;
+use crate::Result;
+use crate::call::Call;
+use crate::query::Query;
+
+pub trait Client<T: Clone> {
+    type Client: Clone;
 
     fn create_client(parent: T) -> Self::Client;
 }
 
-impl<T: Client<U>, U> Client<U> for Result<T> {
+impl<T: Clone> Client<T> for () {
+    type Client = T;
+
+    fn create_client(parent: T) -> Self::Client {
+        parent
+    }
+}
+
+impl<T: Client<U>, U: Clone> Client<U> for &T {
     type Client = T::Client;
 
     fn create_client(parent: U) -> Self::Client {
@@ -14,7 +28,7 @@ impl<T: Client<U>, U> Client<U> for Result<T> {
     }
 }
 
-impl<T: Client<U>, U> Client<U> for Option<T> {
+impl<T: Client<U>, U: Clone> Client<U> for &mut T {
     type Client = T::Client;
 
     fn create_client(parent: U) -> Self::Client {
@@ -22,7 +36,7 @@ impl<T: Client<U>, U> Client<U> for Option<T> {
     }
 }
 
-impl<T: Client<U>, U> Client<U> for &T {
+impl<T: Client<U>, U: Clone> Client<U> for Result<T> {
     type Client = T::Client;
 
     fn create_client(parent: U) -> Self::Client {
@@ -30,7 +44,7 @@ impl<T: Client<U>, U> Client<U> for &T {
     }
 }
 
-impl<T: Client<U>, U> Client<U> for &mut T {
+impl<T: Client<U>, U: Clone> Client<U> for Option<T> {
     type Client = T::Client;
 
     fn create_client(parent: U) -> Self::Client {
@@ -44,64 +58,10 @@ mod tests {
 
     use super::*;
     use crate::call::Call;
+    use crate::query::Query;
     use crate::encoding::{Decode, Encode};
     use std::cell::RefCell;
     use std::rc::Rc;
-
-    #[derive(Debug)]
-    pub struct Signer<T> {
-        inner: T,
-    }
-    impl<T: Call> Call for Signer<T>
-    where
-        T::Call: Debug,
-    {
-        type Call = (u32, T::Call);
-
-        fn call(&mut self, call: Self::Call) -> Result<()> {
-            println!("Called call({:?}) on Signer", &call);
-
-            let (signature, subcall) = call;
-            if signature != 123 {
-                failure::bail!("Invalid signature");
-            }
-
-            self.inner.call(subcall)
-        }
-    }
-    impl<T: Client<SignerClient<T, U>>, U: Clone> Client<U> for Signer<T> {
-        type Client = T::Client;
-
-        fn create_client(parent: U) -> Self::Client {
-            T::create_client(SignerClient {
-                parent: parent.clone(),
-                marker: std::marker::PhantomData,
-            })
-        }
-    }
-    pub struct SignerClient<T, U: Clone> {
-        parent: U,
-        marker: std::marker::PhantomData<T>,
-    }
-    impl<T, U: Clone> Clone for SignerClient<T, U> {
-        fn clone(&self) -> Self {
-            SignerClient {
-                parent: self.parent.clone(),
-                marker: std::marker::PhantomData,
-            }
-        }
-    }
-    impl<T: Call, U: Call<Call = (u32, T::Call)> + Clone> Call for SignerClient<T, U>
-    where
-        T::Call: Debug,
-    {
-        type Call = T::Call;
-
-        fn call(&mut self, call: Self::Call) -> Result<()> {
-            println!("called call({:?}) on SignerClient, adding signature", &call);
-            self.parent.call((123, call))
-        }
-    }
 
     #[derive(Debug)]
     pub struct Foo {
@@ -109,6 +69,15 @@ mod tests {
         pub bar2: Bar,
     }
     impl Foo {
+        pub fn get_bar(&self, id: u8) -> Result<&Bar> {
+            println!("Called get_bar({}) on Foo", id);
+            match id {
+                0 => Ok(&self.bar),
+                1 => Ok(&self.bar2),
+                _ => failure::bail!("Invalid id"),
+            }
+        }
+
         pub fn get_bar_mut(&mut self, id: u8) -> Result<&mut Bar> {
             println!("Called get_bar_mut({}) on Foo", id);
             match id {
@@ -122,20 +91,39 @@ mod tests {
     pub enum FooCall {
         Bar(BarCall),
         Bar2(BarCall),
-        GetBar(u8, BarCall),
+        GetBarMut(u8, BarCall),
     }
     impl Call for Foo {
         type Call = FooCall;
 
         fn call(&mut self, call: Self::Call) -> Result<()> {
-            println!("called call({:?} on Foo", &call);
+            println!("called call({:?}) on Foo", &call);
             match call {
                 FooCall::Bar(call) => self.bar.call(call),
                 FooCall::Bar2(call) => self.bar2.call(call),
-                FooCall::GetBar(id, call) => self.get_bar_mut(id)?.call(call),
+                FooCall::GetBarMut(id, call) => self.get_bar_mut(id).call(call),
             }
         }
     }
+    #[derive(Debug, Encode, Decode)]
+    pub enum FooQuery {
+        Bar(BarQuery),
+        Bar2(BarQuery),
+        GetBar(u8, BarQuery),
+    }
+    impl Query for Foo {
+        type Query = FooQuery;
+
+        fn query(&self, query: Self::Query) -> Result<()> {
+            println!("called query({:?}) on Foo", &query);
+            match query {
+                FooQuery::Bar(query) => self.bar.query(query),
+                FooQuery::Bar2(query) => self.bar2.query(query),
+                FooQuery::GetBar(id, query) => self.get_bar(id).query(query),
+            }
+        }
+    } 
+
     impl<T: Clone> Client<T> for Foo {
         type Client = FooClient<T>;
 
@@ -156,7 +144,6 @@ mod tests {
     #[derive(Clone)]
     pub struct FooClient<T> {
         parent: T,
-        // instance: Option<Foo>,
         pub bar: BarClient<BarAdapter<T>>,
         pub bar2: BarClient<Bar2Adapter<T>>,
     }
@@ -164,13 +151,13 @@ mod tests {
         pub fn get_bar_mut(
             &mut self,
             id: u8,
-        ) -> <Result<&mut Bar> as Client<GetBarAdapter<T>>>::Client {
+        ) -> <Result<&mut Bar> as Client<GetBarMutAdapter<T>>>::Client {
             println!("called get_bar_mut({}) on FooClient", id);
-            let adapter = GetBarAdapter {
+            let adapter = GetBarMutAdapter {
                 args: (id,),
                 parent: self.parent.clone(),
             };
-            <Result<&mut Bar> as Client<GetBarAdapter<T>>>::create_client(adapter)
+            <Result<&mut Bar> as Client<GetBarMutAdapter<T>>>::create_client(adapter)
         }
     }
 
@@ -207,19 +194,19 @@ mod tests {
     }
 
     #[derive(Clone)]
-    pub struct GetBarAdapter<T> {
+    pub struct GetBarMutAdapter<T> {
         args: (u8,),
         parent: T,
     }
-    impl<T: Call<Call = FooCall>> Call for GetBarAdapter<T> {
+    impl<T: Call<Call = FooCall>> Call for GetBarMutAdapter<T> {
         type Call = BarCall;
 
         fn call(&mut self, call: Self::Call) -> Result<()> {
             println!(
-                "called call({:?}) on GetBarAdapter, wrapping with FooCall::GetBar({}, ..)",
+                "called call({:?}) on GetBarMutAdapter, wrapping with FooCall::GetBarMut({}, ..)",
                 &call, self.args.0
             );
-            self.parent.call(FooCall::GetBar(self.args.0, call))
+            self.parent.call(FooCall::GetBarMut(self.args.0, call))
         }
     }
 
@@ -229,6 +216,11 @@ mod tests {
         pub fn increment(&mut self) {
             println!("called increment() on Bar");
             self.0 += 1;
+        }
+
+        pub fn count(&self) -> u32 {
+            println!("called count() on Bar");
+            self.0
         }
     }
     #[derive(Debug, Encode, Decode)]
@@ -246,7 +238,22 @@ mod tests {
             Ok(())
         }
     }
-    impl<T> Client<T> for Bar {
+    #[derive(Debug, Encode, Decode)]
+    pub enum BarQuery {
+        Count,
+    }
+    impl Query for Bar {
+        type Query = BarQuery;
+
+        fn query(&self, query: Self::Query) -> Result<()> {
+            println!("called query({:?}) on Bar", &query);
+            match query {
+                BarQuery::Count => self.count(),
+            };
+            Ok(())
+        }
+    }
+    impl<T: Clone> Client<T> for Bar {
         type Client = BarClient<T>;
 
         fn create_client(parent: T) -> Self::Client {
@@ -257,7 +264,6 @@ mod tests {
     #[derive(Clone)]
     pub struct BarClient<T> {
         parent: T,
-        // instance: Option<Bar>,
     }
     impl<T: Call<Call = BarCall>> BarClient<T> {
         pub fn increment(&mut self) -> Result<()> {
@@ -268,18 +274,21 @@ mod tests {
 
     #[test]
     fn client() {
-        let state = Rc::new(RefCell::new(Signer {
-            inner: Foo {
-                bar: Bar(0),
-                bar2: Bar(0),
-            },
+        let state = Rc::new(RefCell::new(Foo {
+            bar: Bar(0),
+            bar2: Bar(0),
         }));
-        let mut client = Signer::<Foo>::create_client(state.clone());
+        let mut client = Foo::create_client(state.clone());
 
         client.bar.increment().unwrap();
-        println!("{:?}\n", &state.borrow());
+        println!("{:?}\n\n", &state.borrow());
 
         client.get_bar_mut(1).increment().unwrap();
-        println!("{:?}", &state.borrow());
+        println!("{:?}\n\n", &state.borrow());
+
+        // println!("{:?}\n\n", client.bar.count());
+
+        // // println!("{:?}\n\n", client.get_bar_count(1));
+        // println!("{:?}\n\n", client.get_bar(1).count());
     }
 }
