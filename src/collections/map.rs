4e635a6fee6d49d467d963bd82c5cd560135c9ed
Matt Bell <mappum@gmail.com>
2021-09-21 14:32:18 -0500

    Derive Call for Map

--- src/collections/map.rs
+++ src/collections/map.rs
@@ -85,7 +85,7 @@ impl<K> Eq for MapKey<K> {}
 /// When values in the map are mutated, inserted, or deleted, they are retained
 /// in an in-memory map until the call to `State::flush` which writes the
 /// changes to the backing store.
-#[derive(Query)]
+#[derive(Query, Call)]
 pub struct Map<K, V, S = DefaultBackingStore> {
     store: Store<S>,
     children: BTreeMap<MapKey<K>, Option<V>>,
