9ae8918dd778570e30043dae21725a86008e630c
Charlie Little <cwlittle@utexas.edu>
2021-09-28 16:27:36 -0500

    Replace failure::Error with thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,10 @@
-use failure::Error;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Unknown Error")]
+    Unknown,
+}
 
 /// A result type bound to the standard orga error type.    
 pub type Result<T> = std::result::Result<T, Error>;
