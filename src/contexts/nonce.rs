06073f45d35766c23bf963270f175bdbd130bad0
Charlie Little <cwlittle@utexas.edu>
2021-10-12 23:29:45 -0500

    Uncomment nonce context tests

--- src/contexts/nonce.rs
+++ src/contexts/nonce.rs
@@ -203,82 +203,82 @@ fn write_nonce(nonce: u64) -> Result<()> {
 
 #[cfg(test)]
 mod tests {
-    // use super::super::Signer;
-    // use super::*;
-    // use crate::contexts::Context;
-    // use crate::store::{MapStore, Shared, Store};
+    use super::super::Signer;
+    use super::*;
+    use crate::contexts::Context;
+    use crate::store::{MapStore, Shared, Store};
 
-    // #[derive(State)]
-    // struct Counter {
-    //     pub count: u64,
-    // }
+    #[derive(State)]
+    struct Counter {
+        pub count: u64,
+    }
 
-    // impl Counter {
-    //     fn increment(&mut self) -> Result<()> {
-    //         self.count += 1;
+    impl Counter {
+        fn increment(&mut self) -> Result<()> {
+            self.count += 1;
 
-    //         Ok(())
-    //     }
-    // }
+            Ok(())
+        }
+    }
 
-    // #[derive(Encode, Decode)]
-    // enum CounterCall {
-    //     Increment,
-    // }
+    #[derive(Encode, Decode)]
+    enum CounterCall {
+        Increment,
+    }
 
-    // impl Call for Counter {
-    //     type Call = CounterCall;
+    impl Call for Counter {
+        type Call = CounterCall;
 
-    //     fn call(&mut self, _call: Self::Call) -> Result<()> {
-    //         self.increment()
-    //     }
-    // }
+        fn call(&mut self, _call: Self::Call) -> Result<()> {
+            self.increment()
+        }
+    }
 
-    // fn nonced_call(n: u64) -> NonceCall<CounterCall> {
-    //     NonceCall {
-    //         nonce: Some(n),
-    //         inner_call: CounterCall::Increment,
-    //     }
-    // }
+    fn nonced_call(n: u64) -> NonceCall<CounterCall> {
+        NonceCall {
+            nonce: Some(n),
+            inner_call: CounterCall::Increment,
+        }
+    }
 
-    // fn unnonced_call() -> NonceCall<CounterCall> {
-    //     NonceCall {
-    //         nonce: None,
-    //         inner_call: CounterCall::Increment,
-    //     }
-    // }
+    fn unnonced_call() -> NonceCall<CounterCall> {
+        NonceCall {
+            nonce: None,
+            inner_call: CounterCall::Increment,
+        }
+    }
 
-    // #[test]
-    // fn nonced_calls() {
-    //     let store = Shared::new(MapStore::new());
-    //     let mut state =
-    //         NonceProvider::<Counter>::create(Store::new(store.into()), Default::default()).unwrap();
+    #[test]
+    fn nonced_calls() {
+        let store = Shared::new(MapStore::new());
+        let mut state =
+            NonceProvider::<Counter>::create(Store::new(store.into()), Default::default()).unwrap();
 
-    //     // Fails if the signer context isn't available.
-    //     assert!(state.call(unnonced_call()).is_err());
+        // Fails if the signer context isn't available.
+        assert!(state.call(unnonced_call()).is_err());
 
-    //     Context::add(Signer { signer: None });
-    //     // No signature, no nonce
-    //     state.call(unnonced_call()).unwrap();
+        Context::add(Signer { signer: None });
+        // No signature, no nonce
+        state.call(unnonced_call()).unwrap();
 
-    //     // No signature, with nonce
-    //     assert!(state.call(nonced_call(0)).is_err());
+        // No signature, with nonce
+        assert!(state.call(nonced_call(0)).is_err());
 
-    //     assert_eq!(state.inner.count, 1);
-    //     Context::remove::<Signer>();
-    //     Context::add(Signer {
-    //         signer: Some([0; 32].into()),
-    //     });
+        assert_eq!(state.inner.count, 1);
+        Context::remove::<Signer>();
+        Context::add(Signer {
+            signer: Some([0; 32].into()),
+        });
 
-    //     // Signed, correct nonce
-    //     state.call(nonced_call(0)).unwrap();
-    //     assert_eq!(state.inner.count, 2);
+        // Signed, correct nonce
+        state.call(nonced_call(0)).unwrap();
+        assert_eq!(state.inner.count, 2);
 
-    //     // Signed, incorrect nonce
-    //     assert!(state.call(nonced_call(0)).is_err());
+        // Signed, incorrect nonce
+        assert!(state.call(nonced_call(0)).is_err());
 
-    //     // Signed, no nonce
-    //     assert!(state.call(unnonced_call()).is_err());
-    //     Context::remove::<Signer>();
-    // }
+        // Signed, no nonce
+        assert!(state.call(unnonced_call()).is_err());
+        Context::remove::<Signer>();
+    }
 }
