a820f49bb53d2d24ad8b7b4a75da1ee30d769e5d
Charlie Little <cwlittle@utexas.edu>
2022-08-10 16:56:45 -0500

    Update GRPC connection for with_response

--- src/ibc/grpc/connection.rs
+++ src/ibc/grpc/connection.rs
@@ -11,19 +11,22 @@ use ibc_proto::ibc::core::connection::v1::{
 use std::str::FromStr;
 
 use super::Ibc;
-use crate::client::{AsyncCall, AsyncQuery, Call};
+use crate::abci::tendermint_client::{TendermintAdapter, TendermintClient};
+use crate::client::{AsyncCall, AsyncQuery, Call, Client};
 use crate::query::Query;
 use std::rc::Rc;
 use tonic::{Request, Response, Status};
 
 #[tonic::async_trait]
-impl<T> ConnectionQuery for super::GrpcServer<T>
+impl<T, U> ConnectionQuery for super::GrpcServer<T, U>
 where
     T: Clone + Send + Sync + 'static,
     // T: AsyncCall<Call = <Ibc as Call>::Call>,
     T: AsyncQuery,
     T: for<'a> AsyncQuery<Response<'a> = Rc<Ibc>>,
     T: AsyncQuery<Query = <Ibc as Query>::Query>,
+    U: Client<TendermintAdapter<U>>,
+    <U as Client<TendermintAdapter<U>>>::Client: Sync + Send,
 {
     async fn connection(
         &self,
