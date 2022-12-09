f6484fa8bad4df5df6e8ea0198cac4441cb8f65c
Judd Keppel <kepzorz@gmail.com>
2022-12-09 12:09:40 -0600

    Remove Store parameter from collections

--- src/collections/entry_map.rs
+++ src/collections/entry_map.rs
@@ -18,26 +18,20 @@ use crate::Result;
 
 #[derive(Query, Call, Encode, Decode, Serialize, Deserialize)]
 #[serde(bound = "")]
-pub struct EntryMap<T: Entry, S: Default = DefaultBackingStore> {
-    map: Map<T::Key, T::Value, S>,
+pub struct EntryMap<T: Entry> {
+    map: Map<T::Key, T::Value>,
 }
 
-impl<T: Entry, S: Default> State<S> for EntryMap<T, S>
+impl<T: Entry> State for EntryMap<T>
 where
     T::Key: Encode + Terminated,
-    T::Value: State<S>,
+    T::Value: State,
 {
-    fn attach(&mut self, store: Store<S>) -> Result<()>
-    where
-        S: Read,
-    {
+    fn attach(&mut self, store: Store) -> Result<()> {
         self.map.attach(store)
     }
 
-    fn flush(&mut self) -> Result<()>
-    where
-        S: Write,
-    {
+    fn flush(&mut self) -> Result<()> {
         self.map.flush()
     }
 }
@@ -56,7 +50,7 @@ where
     }
 }
 
-impl<T: Entry, S: Default> Default for EntryMap<T, S> {
+impl<T: Entry> Default for EntryMap<T> {
     fn default() -> Self {
         Self {
             map: Map::default(),
@@ -64,18 +58,18 @@ impl<T: Entry, S: Default> Default for EntryMap<T, S> {
     }
 }
 
-impl<T: Entry, S: Default> EntryMap<T, S> {
+impl<T: Entry> EntryMap<T> {
     pub fn new() -> Self {
         Self::default()
     }
 }
 
-impl<T: Entry, S: Default + Read> EntryMap<T, S>
+impl<T: Entry> EntryMap<T>
 where
     T::Key: Encode + Terminated,
-    T::Value: State<S>,
+    T::Value: State,
 {
-    pub fn with_store(store: Store<S>) -> Result<Self> {
+    pub fn with_store(store: Store) -> Result<Self> {
         Ok(Self {
             map: Map::with_store(store)?,
         })
@@ -85,12 +79,11 @@ where
 // TODO: add a get_mut method (maybe just takes in T::Key?) so we can add
 // #[call] to it to route calls to children
 
-impl<T, S: Default> EntryMap<T, S>
+impl<T> EntryMap<T>
 where
     T: Entry,
     T::Key: Encode + Terminated,
-    T::Value: State<S>,
-    S: Read,
+    T::Value: State,
 {
     pub fn insert(&mut self, entry: T) -> Result<()> {
         let (key, value) = entry.into_entry();
@@ -104,12 +97,11 @@ where
     }
 }
 
-impl<T, S: Default> EntryMap<T, S>
+impl<T> EntryMap<T>
 where
     T: Entry,
     T::Key: Encode + Terminated + Clone,
-    T::Value: State<S>,
-    S: Read,
+    T::Value: State,
 {
     pub fn delete(&mut self, entry: T) -> Result<()> {
         let (key, _) = entry.into_entry();
@@ -119,12 +111,11 @@ where
     }
 }
 
-impl<T, S: Default> EntryMap<T, S>
+impl<T> EntryMap<T>
 where
     T: Entry,
     T::Key: Encode + Terminated + Clone,
-    T::Value: State<S> + Eq,
-    S: Read,
+    T::Value: State + Eq,
 {
     #[query]
     pub fn contains(&self, entry: T) -> Result<bool> {
@@ -146,39 +137,36 @@ where
     }
 }
 
-impl<'a, T: Entry, S: Default> EntryMap<T, S>
+impl<'a, T: Entry> EntryMap<T>
 where
     T::Key: Next + Decode + Encode + Terminated + Clone,
-    T::Value: State<S> + Clone,
-    S: Read,
+    T::Value: State + Clone,
 {
-    pub fn iter(&'a self) -> Result<Iter<'a, T, S>> {
+    pub fn iter(&'a self) -> Result<Iter<'a, T>> {
         Ok(Iter {
             map_iter: self.map.iter()?,
         })
     }
 
-    pub fn range<B: RangeBounds<T::Key>>(&'a self, range: B) -> Result<Iter<'a, T, S>> {
+    pub fn range<B: RangeBounds<T::Key>>(&'a self, range: B) -> Result<Iter<'a, T>> {
         Ok(Iter {
             map_iter: self.map.range(range)?,
         })
     }
 }
 
-pub struct Iter<'a, T: Entry, S: Default>
+pub struct Iter<'a, T: Entry>
 where
     T::Key: Next + Decode + Encode + Terminated + Clone,
-    T::Value: State<S> + Clone,
-    S: Read,
+    T::Value: State + Clone,
 {
-    map_iter: MapIter<'a, T::Key, T::Value, S>,
+    map_iter: MapIter<'a, T::Key, T::Value>,
 }
 
-impl<'a, T: Entry, S: Default> Iterator for Iter<'a, T, S>
+impl<'a, T: Entry> Iterator for Iter<'a, T>
 where
     T::Key: Next + Decode + Encode + Terminated + Clone,
-    T::Value: State<S> + Clone,
-    S: Read,
+    T::Value: State + Clone,
 {
     type Item = Result<ReadOnly<T>>;
 
