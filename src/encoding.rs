4c9dbc2805baf710e970bfc1e4fabcc2125496ea
Judd Keppel <kepzorz@gmail.com>
2023-01-20 15:56:32 -0600

    Update LengthVec state implementation to use inner type's encoding

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,6 +1,7 @@
 use crate::migrate::MigrateFrom;
 use crate::query::Query;
 use crate::state::State;
+use crate::store::Store;
 use crate::{client::Client, describe::Describe};
 pub use ed::*;
 pub use orga_macros::VersionedEncoding;
@@ -8,28 +9,14 @@ pub mod decoder;
 pub mod encoder;
 
 use derive_more::{Deref, DerefMut, Into};
-use serde::{Deserialize, Serialize};
 use std::convert::{TryFrom, TryInto};
+use std::io::Read;
 
-#[derive(
-    Deref,
-    DerefMut,
-    Encode,
-    Into,
-    Default,
-    Clone,
-    Debug,
-    State,
-    Query,
-    Client,
-    Serialize,
-    Deserialize,
-    MigrateFrom,
-)]
+#[derive(Deref, DerefMut, Encode, Into, Default, Clone, Debug, Query, MigrateFrom)]
 pub struct LengthVec<P, T>
 where
-    P: State + Encode + Decode + TryInto<usize> + Terminated + Clone,
-    T: State + Encode + Decode + Terminated,
+    P: Encode + Decode + TryInto<usize> + Terminated + Clone,
+    T: Encode + Decode + Terminated,
 {
     len: P,
 
@@ -41,8 +28,8 @@ where
 
 impl<P, T> LengthVec<P, T>
 where
-    P: State + Encode + Decode + TryInto<usize> + Terminated + Clone,
-    T: State + Encode + Decode + Terminated,
+    P: Encode + Decode + TryInto<usize> + Terminated + Clone,
+    T: Encode + Decode + Terminated,
 {
     pub fn new(len: P, values: Vec<T>) -> Self {
         LengthVec { len, values }
@@ -51,11 +38,12 @@ where
 
 impl<P, T> Decode for LengthVec<P, T>
 where
-    P: State + Encode + Decode + Terminated + TryInto<usize> + Clone,
-    T: State + Encode + Decode + Terminated,
+    P: Encode + Decode + Terminated + TryInto<usize> + Clone,
+    T: Encode + Decode + Terminated,
 {
     fn decode<R: std::io::Read>(mut input: R) -> Result<Self> {
         let len = P::decode(&mut input)?;
+
         let len_usize = len
             .clone()
             .try_into()
@@ -73,20 +61,53 @@ where
 
 impl<P, T> Terminated for LengthVec<P, T>
 where
-    P: State + Encode + Decode + TryInto<usize> + Terminated + Clone,
-    T: State + Encode + Decode + Terminated,
+    P: Encode + Decode + TryInto<usize> + Terminated + Clone,
+    T: Encode + Decode + Terminated,
 {
 }
 
-impl<P, T> Describe for LengthVec<P, T>
+impl<P, T> State for LengthVec<P, T>
 where
-    P: State + Encode + Decode + TryInto<usize> + Terminated + Clone + 'static,
-    T: State + Encode + Decode + Terminated + 'static,
+    P: Encode + Decode + TryInto<usize> + Terminated + Clone,
+    T: Encode + Decode + Terminated,
 {
-    fn describe() -> crate::describe::Descriptor {
-        crate::describe::Builder::new::<Self>().build()
+    fn attach(&mut self, _store: Store) -> crate::Result<()> {
+        Ok(())
+    }
+
+    fn flush<W: std::io::Write>(self, out: &mut W) -> crate::Result<()> {
+        self.len.encode_into(out)?;
+        self.values.encode_into(out)?;
+
+        Ok(())
+    }
+
+    fn load(store: Store, mut bytes: &mut &[u8]) -> crate::Result<Self> {
+        let len = P::decode(&mut bytes)?;
+        let len_usize = len
+            .clone()
+            .try_into()
+            .map_err(|_| Error::UnexpectedByte(80))?;
+
+        let mut values = Vec::with_capacity(len_usize);
+        for _ in 0..len_usize {
+            let value = T::decode(&mut bytes)?;
+            values.push(value);
+        }
+
+        Ok(LengthVec { len, values })
     }
 }
+
+// impl<P, T> Describe for LengthVec<P, T>
+// where
+//     P: State + Encode + Decode + TryInto<usize> + Terminated + Clone + 'static,
+//     T: State + Encode + Decode + Terminated + 'static,
+// {
+//     fn describe() -> crate::describe::Descriptor {
+//         crate::describe::Builder::new::<Self>().build()
+//     }
+// }
 
 impl<P, T> TryFrom<Vec<T>> for LengthVec<P, T>
 where
