3f5ad6b67e27837d5e39afa83d727ba0a72d2a2f
Matt Bell <mappum@gmail.com>
2023-02-03 15:14:27 -0600

    Clean up Validators context in drop, to ensure it is cleaned up if a panic happens during its lifetime

--- src/plugins/abci.rs
+++ src/plugins/abci.rs
@@ -264,6 +264,7 @@ mod full {
             use ABCICall::*;
             let validators =
                 Validators::new(self.current_vp.clone(), self.cons_key_by_op_addr.clone());
+            let context_remover = ContextRemover;
             Context::add(validators);
             let create_time_ctx = |time: &Option<Timestamp>| {
                 if let Some(timestamp) = time {
@@ -335,8 +336,22 @@ mod full {
             }
 
             self.build_updates()?;
-            Context::remove::<Validators>();
+            context_remover.remove();
             Ok(())
+        }
+    }
+
+    struct ContextRemover;
+
+    impl ContextRemover {
+        fn remove(&self) {
+            Context::remove::<Validators>();
+        }
+    }
+
+    impl Drop for ContextRemover {
+        fn drop(&mut self) {
+            self.remove();
         }
     }
 
