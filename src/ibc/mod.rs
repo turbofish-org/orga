05793c69513279ab2d088b5eff9eed485200fdb8
Matt Bell <mappum@gmail.com>
2023-04-10 12:42:58 -0500

    Add conversions for PortChannel and PortChannelSequence, and add rest of IBC state to test

--- src/ibc/mod.rs
+++ src/ibc/mod.rs
@@ -2,9 +2,14 @@ use ibc::core::ics04_channel::packet::Sequence;
 use ibc::core::ics24_host::identifier::{
     ChannelId, ClientId as IbcClientId, ConnectionId as IbcConnectionId, PortId,
 };
+use ibc::core::ics24_host::path::{
+    AcksPath, ChannelEndsPath, CommitmentsPath, ReceiptsPath, SeqAcksPath, SeqRecvsPath,
+    SeqSendsPath,
+};
+use serde::Serialize;
 
 use crate::collections::Map;
-use crate::encoding::{ByteTerminatedString, EofTerminatedString};
+use crate::encoding::{ByteTerminatedString, Decode, Encode, EofTerminatedString, FixedString};
 use crate::orga;
 
 // "clients/{identifier}/clientState"	ClientState	ICS 2
@@ -57,18 +62,70 @@ pub struct Client {
     consensus_states: Map<Height, ConsensusState>,
 }
 
-pub type ClientId = ByteTerminatedString<b'/', IbcClientId>;
+pub type SlashTerminatedString<T> = ByteTerminatedString<b'/', T>;
+
+pub type ClientId = SlashTerminatedString<IbcClientId>;
 pub type ConnectionId = EofTerminatedString<IbcConnectionId>;
-pub type PortChannel = (
-    ByteTerminatedString<b'/', PortId>,
+pub type Height = EofTerminatedString<u64>;
+
+#[derive(Encode, Decode, Serialize, Clone, Debug)]
+pub struct PortChannel(
+    #[serde(skip)] FixedString<"ports/">,
+    SlashTerminatedString<PortId>,
+    #[serde(skip)] FixedString<"channels/">,
     EofTerminatedString<ChannelId>,
 );
-pub type PortChannelSequence = (
-    ByteTerminatedString<b'/', PortId>,
-    ByteTerminatedString<b'/', ChannelId>,
+
+macro_rules! port_channel_from_impl {
+    ($ty:ty) => {
+        impl From<$ty> for PortChannel {
+            fn from(path: $ty) -> Self {
+                Self(
+                    FixedString,
+                    ByteTerminatedString(path.0),
+                    FixedString,
+                    EofTerminatedString(path.1),
+                )
+            }
+        }
+    };
+}
+
+port_channel_from_impl!(ChannelEndsPath);
+port_channel_from_impl!(SeqSendsPath);
+port_channel_from_impl!(SeqRecvsPath);
+port_channel_from_impl!(SeqAcksPath);
+
+#[derive(Encode, Decode, Serialize, Clone, Debug)]
+pub struct PortChannelSequence(
+    #[serde(skip)] FixedString<"ports/">,
+    SlashTerminatedString<PortId>,
+    #[serde(skip)] FixedString<"channels/">,
+    SlashTerminatedString<ChannelId>,
+    #[serde(skip)] FixedString<"sequences/">,
     EofTerminatedString<Sequence>,
 );
-pub type Height = EofTerminatedString<u64>;
+
+macro_rules! port_channel_sequence_from_impl {
+    ($ty:ty) => {
+        impl From<$ty> for PortChannelSequence {
+            fn from(path: $ty) -> Self {
+                Self(
+                    FixedString,
+                    ByteTerminatedString(path.port_id),
+                    FixedString,
+                    ByteTerminatedString(path.channel_id),
+                    FixedString,
+                    EofTerminatedString(path.sequence),
+                )
+            }
+        }
+    };
+}
+
+port_channel_sequence_from_impl!(CommitmentsPath);
+port_channel_sequence_from_impl!(AcksPath);
+port_channel_sequence_from_impl!(ReceiptsPath);
 
 // TODO
 pub type ClientState = ();
@@ -102,23 +159,53 @@ mod tests {
 
         let mut client = Client::default();
         client.client_state.insert((), ()).unwrap();
-        client.consensus_states.insert(1.into(), ()).unwrap();
-        client.consensus_states.insert(2.into(), ()).unwrap();
+        client.consensus_states.insert(10.into(), ()).unwrap();
+        client.consensus_states.insert(20.into(), ()).unwrap();
         let client_id = IbcClientId::new(ClientType::Tendermint, 123)
             .unwrap()
             .into();
         ibc.clients.insert(client_id, client).unwrap();
 
-        let mut client = Client::default();
-        client.client_state.insert((), ()).unwrap();
-        client.consensus_states.insert(10.into(), ()).unwrap();
-        client.consensus_states.insert(20.into(), ()).unwrap();
-        let client_id = IbcClientId::new(ClientType::Tendermint, 124)
-            .unwrap()
-            .into();
-        ibc.clients.insert(client_id, client).unwrap();
+        let conn_id = IbcConnectionId::new(123).into();
+        ibc.connections.insert(conn_id, ()).unwrap();
 
-        // TODO: fill in rest of state
+        let channel_end_path = ChannelEndsPath(PortId::transfer(), ChannelId::new(123)).into();
+        ibc.channel_ends.insert(channel_end_path, ()).unwrap();
+
+        let seq_sends_path = SeqSendsPath(PortId::transfer(), ChannelId::new(123)).into();
+        ibc.next_sequence_send.insert(seq_sends_path, 1).unwrap();
+
+        let seq_recvs_path = SeqRecvsPath(PortId::transfer(), ChannelId::new(123)).into();
+        ibc.next_sequence_recv.insert(seq_recvs_path, 2).unwrap();
+
+        let seq_acks_path = SeqAcksPath(PortId::transfer(), ChannelId::new(123)).into();
+        ibc.next_sequence_ack.insert(seq_acks_path, 3).unwrap();
+
+        let commitments_path = CommitmentsPath {
+            port_id: PortId::transfer(),
+            channel_id: ChannelId::new(123),
+            sequence: 1.into(),
+        }
+        .into();
+        ibc.commitments
+            .insert(commitments_path, vec![1, 2, 3])
+            .unwrap();
+
+        let acks_path = AcksPath {
+            port_id: PortId::transfer(),
+            channel_id: ChannelId::new(123),
+            sequence: 1.into(),
+        }
+        .into();
+        ibc.acks.insert(acks_path, vec![1, 2, 3]).unwrap();
+
+        let receipts_path = ReceiptsPath {
+            port_id: PortId::transfer(),
+            channel_id: ChannelId::new(123),
+            sequence: 1.into(),
+        }
+        .into();
+        ibc.receipts.insert(receipts_path, vec![1, 2, 3]).unwrap();
 
         let mut bytes = vec![];
         app.flush(&mut bytes).unwrap();
@@ -131,14 +218,19 @@ mod tests {
                 String::from_utf8(key.to_vec()).unwrap()
             );
         };
+
+        assert_next_key(b"acks/ports/transfer/channels/channel-123/sequences/1");
+        assert_next_key(b"channelEnds/ports/transfer/channels/channel-123");
         assert_next_key(b"clients/07-tendermint-123/");
         assert_next_key(b"clients/07-tendermint-123/clientState");
-        assert_next_key(b"clients/07-tendermint-123/consensusStates/1");
-        assert_next_key(b"clients/07-tendermint-123/consensusStates/2");
-        assert_next_key(b"clients/07-tendermint-124/");
-        assert_next_key(b"clients/07-tendermint-124/clientState");
-        assert_next_key(b"clients/07-tendermint-124/consensusStates/10");
-        assert_next_key(b"clients/07-tendermint-124/consensusStates/20");
+        assert_next_key(b"clients/07-tendermint-123/consensusStates/10");
+        assert_next_key(b"clients/07-tendermint-123/consensusStates/20");
+        assert_next_key(b"commitments/ports/transfer/channels/channel-123/sequences/1");
+        assert_next_key(b"connections/connection-123");
+        assert_next_key(b"nextSequenceAck/ports/transfer/channels/channel-123");
+        assert_next_key(b"nextSequenceRecv/ports/transfer/channels/channel-123");
+        assert_next_key(b"nextSequenceSend/ports/transfer/channels/channel-123");
+        assert_next_key(b"receipts/ports/transfer/channels/channel-123/sequences/1");
         assert!(entries.next().is_none());
     }
 }
