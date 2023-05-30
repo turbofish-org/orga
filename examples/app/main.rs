2196bde943699944740850c4d6405899b42a707a
Matt Bell <mappum@gmail.com>
2023-05-30 14:07:15 -0500

    Fix example app

---
+++ examples/app/main.rs
@@ -0,0 +1,51 @@
+#![feature(trivial_bounds)]
+
+use orga::{
+    call::Call,
+    coins::{Accounts, Symbol},
+    collections::Map,
+    orga,
+    plugins::DefaultPlugins,
+    prelude::{ConvertSdkTx, PaidCall},
+};
+
+#[orga]
+#[derive(Debug, Clone, Copy)]
+pub struct FooCoin();
+
+impl Symbol for FooCoin {
+    const INDEX: u8 = 123;
+}
+
+#[orga]
+pub struct App {
+    pub foo: u32,
+    pub bar: u32,
+    pub map: Map<u32, u32>,
+    #[call]
+    pub accounts: Accounts<FooCoin>,
+}
+
+pub fn main() {
+    pretty_env_logger::init();
+
+    let home = tempdir::TempDir::new("orga-foo").unwrap();
+    dbg!(&home);
+    let node = orga::abci::Node::<DefaultPlugins<FooCoin, App>>::new(
+        home.path().clone(),
+        orga::prelude::DefaultConfig {
+            seeds: None,
+            timeout_commit: None,
+        },
+    );
+    node.run().unwrap();
+    home.close().unwrap();
+}
+
+impl ConvertSdkTx for App {
+    type Output = PaidCall<<App as Call>::Call>;
+
+    fn convert(&self, msg: &orga::prelude::sdk_compat::sdk::Tx) -> orga::Result<Self::Output> {
+        todo!()
+    }
+}
