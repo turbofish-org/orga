e94d6a6be70e696e796ac415e12441af4c109c98
Matt Bell <mappum@gmail.com>
2023-05-31 17:16:16 -0500

    Add DerivedKey wallet

--- src/client/wallet.rs
+++ src/client/wallet.rs
@@ -9,10 +9,12 @@ pub trait Wallet: Clone {
 
     fn address(&self) -> Result<Option<Address>>;
 
-    fn nonce_hint(&self) -> Result<Option<u64>>;
+    fn nonce_hint(&self) -> Result<Option<u64>> {
+        Ok(None)
+    }
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default)]
 pub struct Unsigned;
 
 impl Wallet for Unsigned {
@@ -28,9 +30,62 @@ impl Wallet for Unsigned {
     fn address(&self) -> Result<Option<Address>> {
         Ok(None)
     }
+}
 
-    fn nonce_hint(&self) -> Result<Option<u64>> {
-        Ok(None)
+/// A wallet that derives a private key from a seed - intended to be used in
+/// tests.
+#[derive(Clone, Debug)]
+pub struct DerivedKey {
+    privkey: secp256k1::SecretKey,
+}
+
+impl DerivedKey {
+    pub fn new(seed: &[u8]) -> Result<Self> {
+        use sha2::Digest;
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(&seed);
+        let hash = hasher.finalize();
+
+        let privkey = secp256k1::SecretKey::from_slice(&hash)?;
+
+        Ok(Self { privkey })
+    }
+
+    pub fn address_for(seed: &[u8]) -> Result<Address> {
+        Ok(Self::new(seed)?.address())
+    }
+
+    pub fn privkey(&self) -> &secp256k1::SecretKey {
+        &self.privkey
+    }
+
+    pub fn pubkey(&self) -> secp256k1::PublicKey {
+        let secp = secp256k1::Secp256k1::new();
+        secp256k1::PublicKey::from_secret_key(&secp, &self.privkey)
+    }
+
+    pub fn address(&self) -> Address {
+        Address::from_pubkey(self.pubkey().serialize())
+    }
+}
+
+impl Wallet for DerivedKey {
+    fn sign(&self, call_bytes: &[u8]) -> Result<SignerCall> {
+        let secp = secp256k1::Secp256k1::new();
+        let msg = secp256k1::Message::from_slice(call_bytes)?;
+        let sig = secp.sign_ecdsa(&msg, &self.privkey);
+        let sig_bytes = sig.serialize_compact();
+
+        Ok(SignerCall {
+            call_bytes: call_bytes.to_vec(),
+            signature: Some(sig_bytes),
+            pubkey: Some(self.pubkey().serialize()),
+            sigtype: SigType::Native,
+        })
+    }
+
+    fn address(&self) -> Result<Option<Address>> {
+        Ok(Some(self.address()))
     }
 }
 
