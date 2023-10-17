26715f500fbbc9eb0d0bbb790999f1713762793b
Matt Bell <mappum@gmail.com>
2023-10-17 15:30:12 -0500

    Fix encoding::Decoder version byte check

--- src/encoding/decoder.rs
+++ src/encoding/decoder.rs
@@ -21,8 +21,11 @@ impl<R: Read> Decoder<R> {
     where
         U: Decode,
     {
-        if self.field_count == 0 && !compat_mode() && self.version == 0 {
-            let _version_byte = u8::decode(&mut self.bytes)?;
+        if self.field_count == 0 && !compat_mode() {
+            let version_byte = u8::decode(&mut self.bytes)?;
+            if version_byte != self.version {
+                return Err(ed::Error::UnexpectedByte(version_byte));
+            }
         }
         let res = U::decode(&mut self.bytes);
         self.field_count += 1;
